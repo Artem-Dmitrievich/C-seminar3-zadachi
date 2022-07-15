7e40daeb006cdeba804420f690f741d6b290f7a1
kozlov artem <artemko1987@yandex.ru>
2022-07-15 21:11:52 +0300

    Zadacha22

---
+++ Zadacha22/Program.cs
@@ -0,0 +1,13 @@
+﻿// 1 вариант
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= n)
+{
+    Console.Write(i * i + " ");
+    i++;
+}
+
+// 2 вариант
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+    Console.Write(i * i + " ");
